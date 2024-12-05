325a174aaee36906eeedd2dfed722045ce445b58
Nalla Prakash reddy <prakashreddynalla285@gmail.com>
2024-12-05 12:40:37 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  
+}
+resource "aws_instance" "public_ec2" {
+  ami = "ami-038bba9a164eb3dc1"
+  instance_type   = "t2.micro" 
+  key_name = "west"
+  tags = {
+    name="public_ec2"
+  }
+}
